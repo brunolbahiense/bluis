3ec61cf8a309773aea5c27517b359fe942d265dc
Brunolbahiense <brunoluisbahiense@gmail.com>
2021-06-19 11:29:44 -0300

    first commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/Bluis.svg" alt="Bluis escrito entre os síbolos < >" />
+    <S.Description>Em breve</S.Description>
+  </S.Wrapper>
+)
+
+export default Main
