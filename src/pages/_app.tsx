de9b31188c4484f0abaa20476298f1fa91d28580
Brunolbahiense <brunoluisbahiense@gmail.com>
2021-07-21 15:03:51 -0300

    meta updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,28 +14,22 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#06092B" />
         <meta name="Bluis" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
-        <meta property="og:title" content="" />
+        <meta property="og:url" content="https://bluis.vercel.app/" />
+        <meta property="og:title" content="Bluis" />
         <meta
           property="og:description"
-          content="Levando sua marca ao mundo digital"
+          content="Levando sua marca ao mundo digital!"
         />
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="og:image" content="/img/square.svg" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
-        <meta property="twitter:title" content="" />
+        <meta property="twitter:url" content="https://bluis.vercel.app/" />
+        <meta property="twitter:title" content="Bluis" />
         <meta
           property="twitter:description"
-          content="Levando sua marca ao mundo digital"
+          content="Levando sua marca ao mundo digital!"
         />
-        <meta
-          property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="twitter:image" content="/img/square.svg" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
