dd7296d082443a00e41711bd8baee96ce056b047
Brunolbahiense <brunoluisbahiense@gmail.com>
2021-10-14 21:05:49 -0300

    new areas created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-import Main from 'components/Main'
+import * as S from '../styles/Main'
+import SocialMedia from 'components/Social'
 
 export default function Home() {
-  return <Main />
+  return (
+    <>
+      <S.Wrapper>
+        <S.Logo src="/img/Bluis.svg" alt="Bluis escrito entre os síbolos < >" />
+        <S.Description>Soluções em Tech</S.Description>
+      </S.Wrapper>
+      <S.WrapperW>
+        <h1>Meus Projetos</h1>
+        <h2>Esses são os principais projetos desenvolvidos por mim:</h2>
+
+        <h1>Bluis trips</h1>
+        <h1>Tomatracker</h1>
+      </S.WrapperW>
+      <S.Wrapper>
+        <S.Description>Siga-me nas redes Sociais</S.Description>
+        <SocialMedia />
+      </S.Wrapper>
+    </>
+  )
 }
--- src/styles/Main.ts
+++ src/styles/Main.ts
@@ -12,6 +12,18 @@ export const Wrapper = styled.main`
   align-items: center;
   justify-content: center;
 `
+export const WrapperW = styled.main`
+  background-color: #eee;
+  color: #06092b;
+  width: 100%;
+  height: 100%;
+  padding: 2rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 export const Logo = styled.img`
   width: min(50rem, 100%);
